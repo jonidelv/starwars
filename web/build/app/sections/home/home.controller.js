b01578a06967a8c2ffc1d0d9075cac136e92b618
Jonatan del Valle <delvallejonatan@gmail.com>
2016-02-26 00:51:45 -0300

    card generated

--- web/build/app/sections/home/home.controller.js
+++ web/build/app/sections/home/home.controller.js
@@ -5,69 +5,67 @@
       .module('app.home')
       .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['swapi','$state'];
+    HomeController.$inject = ['swapi', '$state'];
 
-    function HomeController(swapi,$state) {
+    function HomeController(swapi, $state) {
       var vm = this;
       vm.selects = {
         names: ['Films', 'People', 'Planets', 'Species', 'Starships', 'Vehicles']
       };
       vm.items = [];
+      vm.itemsMap = {};
       vm.variable = 'name';
 
       vm.onSelectChange = function(option) {
         vm.loading = true;
         swapi[option.toLowerCase()].all()
-          .then( function(response) {
-              vm.items = response.results;
-              vm.father = response.count;
-              vm.loading = false;
+          .then(function(response) {
+            response.results.forEach(function(item) {
+              var property = option === 'Films' ? 'title' : 'name';
+              vm.itemsMap[item[property]] = item;
+              vm.items.push(item);
+            });
+            //vm.items = response.results;
+            vm.father = response.count;
+            vm.loading = false;
           });
       };
 
 
-      vm.selectId = function(option, selected) {
-
-        swapi[option.toLowerCase()].all()
-          .then( function(response) {
-
-              for (var i = 0; i < response.count; i++) {
-                 if (response.results[i].name == selected){
-                   var paso = response.results[i].url.replace(/\D/g,'');
-                  console.log(paso);
-                   //$state.go(option.toLowerCase()({ id:ad  }));
-                   $state.go(option.toLowerCase());
-                 }
-              }
-
-          });
+      vm.selectId = function(selectedName, parentState) {
+        var item = vm.itemsMap[selectedName];
+        var itemId = item.url.replace(/\D/g, '');
+        $state.go(parentState.toLowerCase(), { id:itemId });
+        // for (var i = 0; i < vm.items.length; i++) {
+        //   if (vm.items[i].name === selectedName) {
+        //     var itemId = vm.items[i].url.replace(/\D/g, '');
+        //     $state.go(parentState.toLowerCase(), { id:itemId });
+        //   }
+        // }
       };
 
 
 
-
-
-
-      vm.placeholder = function(){
-        if (!vm.category) {
+        vm.placeholder = function() {
+          if (!vm.category) {
             return 'Select a category';
-        }
-        if (vm.category && vm.loading) {
-          return 'Loading... ';
-        } else {
-          return 'Start typing the '+vm.category;
-        }
-      };
+          }
+          if (vm.category && vm.loading) {
+            return 'Loading... ';
+          } else {
+            return 'Start typing the ' + vm.category;
+          }
+        };
 
-      vm.answer = function (option, item) {
-        if (option === 'Films' ) {
-          return item.title;
-        }
-        return item.name;
-      };
+        vm.answer = function(option, item) {
+          if (option === 'Films') {
+            return item.title;
+          }
+          return item.name;
+        };
 
 
 
-  }
+      }
 
-})();
+    })();
