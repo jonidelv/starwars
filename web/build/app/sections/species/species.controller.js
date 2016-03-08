2a4c74c1e0f5166932e5fe4ef1f2a544fc4a69bf
Jonatan del Valle <delvallejonatan@gmail.com>
2016-03-08 00:02:03 -0300

    routes in cards generated

--- web/build/app/sections/species/species.controller.js
+++ web/build/app/sections/species/species.controller.js
@@ -1,24 +1,43 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app.species')
-        .controller('SpeciesController', SpeciesController);
+  angular
+    .module('app.species')
+    .controller('SpeciesController', SpeciesController);
 
-    SpeciesController.$inject = ['$stateParams', 'swapi'];
+  SpeciesController.$inject = ['$q', '$stateParams', 'swapi'];
 
-    /* @ngInject */
-    function SpeciesController($stateParams, swapi) {
-        var vm = this;
+  /* @ngInject */
+  function SpeciesController($q, $stateParams, swapi) {
+    var vm = this;
+    vm.getId = getId;
 
-        activate();
+    activate();
 
-        function activate() {
-          return swapi.species.id($stateParams.id).then(function(species) {
-            vm.species = species;
-            return species;
-          });
-        }
+    function activate() {
+      return swapi.species.id($stateParams.id).then(function(species) {
+        vm.species = species;
+
+        return swapi.get(vm.species.homeworld);
+      }).then(function(response) {
+        vm.homeworld = response.name;
+
+        var peopleQueue = [];
+        vm.species.people.forEach(function(personUrl) {
+          peopleQueue.push(swapi.get(personUrl));
+        });
+
+        return $q.all(peopleQueue);
+      }).then(function(people) {
+        vm.people = people;
+        return people;
+      });
 
     }
+
+    function getId(url){
+      return url.replace(/\D/g, '');
+    }
+
+  }
 })();
