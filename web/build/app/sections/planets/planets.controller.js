61ddc8fa71a83da47a50be1d47546a33c80b4d20
Jonatan del Valle <delvallejonatan@gmail.com>
2016-03-13 18:28:31 -0300

    styles changed & planets view

--- web/build/app/sections/planets/planets.controller.js
+++ web/build/app/sections/planets/planets.controller.js
@@ -1,24 +1,47 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app.planets')
-        .controller('PlanetsController', PlanetsController);
+  angular
+    .module('app.planets')
+    .controller('PlanetsController', PlanetsController);
 
-    PlanetsController.$inject = ['$stateParams', 'swapi'];
+  PlanetsController.$inject = ['$q', '$stateParams', 'swapi'];
 
-    /* @ngInject */
-    function PlanetsController($stateParams, swapi) {
-        var vm = this;
+  /* @ngInject */
+  function PlanetsController($q, $stateParams, swapi) {
+    var vm = this;
 
-        activate();
+    activate();
 
-        function activate() {
-          return swapi.planets.id($stateParams.id).then(function(planets) {
-            vm.planets = planets;
-            return planets;
-          });
-        }
+    function activate() {
+      return swapi.planets.id($stateParams.id).then(function(planets) {
+        vm.planets = planets;
+
+        var filmsQueue = [];
+        vm.planets.films.forEach(function(filmsUrl) {
+          filmsQueue.push(swapi.get(filmsUrl));
+        });
+
+        return $q.all(filmsQueue);
+      }).then(function(films) {
+        vm.films = films;
+
+        var residentsQueue = [];
+        vm.planets.residents.forEach(function(residentsUrl) {
+          residentsQueue.push(swapi.get(residentsUrl));
+        });
+
+        return $q.all(residentsQueue);
+      }).then(function(response) {
+        vm.residents = response;
+        return vm.residents;
+      });
+
 
     }
+
+    function getId(url) {
+      return url.replace(/\D/g, '');
+    }
+  }
 })();
